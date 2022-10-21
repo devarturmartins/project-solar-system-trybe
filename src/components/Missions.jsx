ec919b7aef6f0a8febc1ad3ab463d5f8bce027cc
devarturmartins <arturvl1@hotmail.com>
2022-10-21 16:58:39 -0300

    projeto concluido sem css

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map((e) => (
+          <MissionCard
+            name={ e.name }
+            year={ e.year }
+            country={ e.country }
+            destination={ e.destination }
+            key={ e.name }
+          />
+        ))}
       </div>
     );
   }
